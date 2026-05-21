6b49be13506df08f77c1e16a3d1b6b12f7cc0758
Majd Kassem <mjdwassouf@gmail.com>
2026-05-21 21:31:21 +0300

    feat: complete environment
    Add config file for simulation and modeling
    
    Adding utils to separat concern

---
+++ environment.py
@@ -0,0 +1,51 @@
+from typing import Any, Dict, Tuple
+
+import numpy as np
+from numpy.typing import NDArray
+
+
+class StochasticEnv:
+    # Type hinting the incoming dictionaries and the RNG generator object
+    def __init__(
+        self, rng: np.random.Generator, env_cfg: Dict[str, Any], sim_cfg: Dict[str, Any]
+    ) -> None:
+        self.rng: np.random.Generator = rng
+        self.dt: float = float(sim_cfg["dt"])
+        self.collision_threshold: float = float(env_cfg["collision_threshold"])
+        self.ego_pos: NDArray[np.float64] = np.array(env_cfg["ego_start"], dtype=float)
+        self.ego_speed: float = float(env_cfg["ego_speed"])
+        self.goal: NDArray[np.float64] = np.array(env_cfg["goal"], dtype=float)
+        self.test = 10
+        self.obstacle_pos: NDArray[np.float64] = np.array(
+            env_cfg["obstacle_start"], dtype=float
+        )
+        self.obstacle_vel: NDArray[np.float64] = np.array(
+            env_cfg["obstacle_velocity"], dtype=float
+        )
+
+    def get_ground_truth_states(
+        self,
+    ) -> Tuple[NDArray[np.float64], NDArray[np.float64]]:
+        return self.ego_pos.copy(), self.obstacle_pos.copy()
+
+    def step(
+        self, steering_angle: float
+    ) -> Tuple[NDArray[np.float64], NDArray[np.float64], bool, bool]:
+        # 1. Deterministic Ego Kinematics
+        self.ego_pos[0] += self.ego_speed * np.cos(steering_angle) * self.dt
+        self.ego_pos[1] += self.ego_speed * np.sin(steering_angle) * self.dt
+        # 2. Stochastic Obstacle Kinematics
+        velocity_drift: NDArray[np.float64] = self.rng.normal(
+            loc=0.0, scale=0.05, size=2
+        )
+        actual_velocity: NDArray[np.float64] = self.obstacle_vel + velocity_drift
+        self.obstacle_pos += actual_velocity * self.dt
+        # 3. Boundary & Termination Checks
+        dist_to_goal: float = float(np.linalg.norm(self.goal - self.ego_pos))
+        dist_to_obstacle: float = float(
+            np.linalg.norm(self.obstacle_pos - self.ego_pos)
+        )
+        collision: bool = dist_to_obstacle < self.collision_threshold
+        reached_goal: bool = dist_to_goal < 0.2
+        done: bool = reached_goal or collision
+        return self.ego_pos.copy(), self.obstacle_pos.copy(), collision, done
